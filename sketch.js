9d8795754d7679468fb3628141c11ba884f176f3
vischeik <114205128+vischeik@users.noreply.github.com>
2022-11-25 13:26:03 +0900

    01
    01

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+let musicFile;
+function setup()
+{
+    createCanvas(200,200);
+    song = loadSong
+    soundFormats('mp3','ogg');
+    musicFile = leadSound('Blast Off',loadMusic);
+    musicFile.play();
+}
+function loadMusic()
+{
+        musicFile.play();
+
+}
