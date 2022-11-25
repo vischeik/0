124199b592509e3d4cc6d06455f6f75e5c6e1730
vischeik <114205128+vischeik@users.noreply.github.com>
2022-11-25 13:55:43 +0900

    01
    03

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
+var song;
+var button;
 
-let musicFile;
-function setup()
-{
-    createCanvas(200,200);
-    song = loadSong
-    soundFormats('mp3','ogg');
-    musicFile = leadSound('Blast Off',loadMusic);
+function setup(){
+        createCanvas(200,200);
+        song = loadSound('Blast Off.mp3',loaded);
+        button = createButton('play')
+        button.mousePressed(togglePlaying);
+        background(51);
 }
-function loadMusic()
-{
-        musicFile.play(); //ekfms vkdlf
 
-}
+function togglePlaying(){
+    if(!song.isPlaying()){
+            song.play();
+            song.setVolume(0.3);
+            button.html('pause');
+    }else{
+    song.stop();
+    button.html('play');
+            }
+    }
+
+    function loaded() {
+        console.log('loaded');
+    }
