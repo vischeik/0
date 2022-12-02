ece61b3e319457312a2ef4b84dea587aa17010ac
vischeik <114205128+vischeik@users.noreply.github.com>
2022-12-02 13:03:32 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,34 @@
 var song;
 var button;
+var jumpButton;
 
 function setup(){
         createCanvas(200,200);
         song = loadSound('Blast Off.mp3',loaded);
         button = createButton('play')
         button.mousePressed(togglePlaying);
+        jumpButton = createButton('jump')
+        jumpButton.mousePressed(jumpSong);
         background(51);
+
+song.addCue(2,changeBackground,color(0,0,0));
+song.addCue(4,changeBackground,color(255,255,255));
+song.addCue(6,changeBackground,color(100,100,100));
+}
+
+function changeBackground(col) {
+        background(col);
+}
+
+function jumpSong() {
+         var len = song.duration();
+         var t = 0;
+         console.log(t);
+         song.jump(t);
+}
+
+function draw() {
+        
 }
 
 function togglePlaying(){
